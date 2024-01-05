1dcf270faa617c1dfc563b0c997e5dad9db39013
rhgarcia88 <rhgarguer@gmail.com>
2024-01-05 12:12:39 +0100

    first commit

---
+++ ej12.js
@@ -0,0 +1,28 @@
+// Crea una función llamada findArrayIndex que reciba como parametros un array de textos y un texto y devuelve la posición del array
+//  cuando el valor del array sea igual al valor del texto que enviaste como parametro.
+
+// Haz varios ejemplos y compruebalos.
+
+
+
+// Ej array:
+const mainCharacters = [
+  "Luke",
+  "Leia",
+  "Han Solo",
+  "Chewbacca",
+  "Rey",
+  "Anakin",
+  "Obi-Wan",
+];
+
+
+function findArrayIndex(array, text) {
+
+return  array.indexOf(text);;
+ 
+}
+
+console.log(findArrayIndex(mainCharacters,'Obi-Wan'));
+console.log(findArrayIndex(mainCharacters,'Leia'));
+console.log(findArrayIndex(mainCharacters,'Rey'));
